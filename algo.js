409f2d48aa7925e50d0f5d11fd5c9a94685a40d1
Manoj <2020uee1424@mnit.ac.in>
2023-05-27 07:42:42 +0530

    initial commit

---
+++ algo.js
@@ -0,0 +1,4 @@
+function algorithm(code1, code2) {
+
+}
+module.exports = algorithm;
---
+++ diff.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const algo = require("./algo.js")
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/diff.html");
+});
+app.post("/", function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    // console.log(req.body);
+    res.send(algo(req.body.code1, req.body.code2));
+})
+app.get("/index.css", function (req, res) {
+    res.set("Content-Type", "text/css");
+    // console.log(req.body);
+    res.sendFile(__dirname + "/index.css");
+});
+app.get("/index.js", function (req, res) {
+    res.set("Content-Type", "text/javascript");
+    // console.log(req.body);
+    res.sendFile(__dirname + "/index.js");
+});
+app.listen(3000);
